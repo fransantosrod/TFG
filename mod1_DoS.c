c0854224968cb0b58023c1fc47fe4e52dfeee3c9
fransantosrod <franciscojavier_95@hotmail.com>
2017-02-12 17:55:34 +0100

    Add files via upload

--- mod1_DoS.c
+++ mod1_DoS.c
@@ -11,20 +11,13 @@ en los ataques DoS
 #include <stdbool.h>
 #include "func_aux.h"
 
-#define IGUAL 0
+
 #define FIN_REGLA_ALERTA 3
 #define SIGUIENTE_LINEA 1
 #define POSICION_IP_ORIGEN 3
 #define POSICION_IP_DESTINO 5
-#define TAM_IP 4
-#define PUNTO '.'
-#define TERMINADOR '\0'
 
-//El número máximo de digitos que puede tener un fragmento de IP
-#define MAX_DIG_FRAGMENTO_IP 3
-
-char **divide_IP(char *direccion_IP);
-
+void comprueba_Coincidencia_Alertas(struct CONTENIDO_FICHERO contenido_del_fichero, char * nombre_Coincidencia);
 
 int main (){
 	
@@ -32,27 +25,57 @@ int main (){
 	FILE *fichero;
 	//Estructura que almacenará los datos relativos al fichero
 	struct CONTENIDO_FICHERO contenido_del_fichero;
+	
+	//Inicializamos las variables
+	fichero = fopen("alert", "r");
+	
+
+	contenido_del_fichero = lee_fichero(fichero);
+	fclose(fichero);
+
+	comprueba_Coincidencia_Alertas(contenido_del_fichero, "DoS");
+
+
+	return 0;
+
+} 	
+
+
+/*---------------------------------------------------------
+	Función que para un string dado comprueba si dicho
+	string está en el contenido leido de un fichero
+	
+	Recibe: Una estructura del tipo CONTENIDO_FICHERO
+	Devuelve: Nada
+
+	Caracteristicas:
+		A través de esta función podremos detectar si 
+		en el fichero hemos leido una alerta de una 
+		regla cuyo mensaje de información coincida 
+		con el string que hemos pasado 
+---------------------------------------------------------*/
+
+void comprueba_Coincidencia_Alertas(struct CONTENIDO_FICHERO contenido_del_fichero, char * nombre_Coincidencia){
+	
 	//Variable para almacenar la línea en la que se ha detectado la coincidencia de DoS
 	int linea_coincidencia;
 	//Bandera para detectar cuando se ha producido una coincidencia
 	bool flag;
-
+	//Bandera para detectar si la red es local
+	bool red_local;
 
 	//Variables para recorrer la tabla con el contenido del fichero
 	int cont_aux_frases_fichero;
 	int cont_aux_palabras_fichero;
 
 	//Inicializamos las variables
-	fichero = fopen("alert", "r");
 	linea_coincidencia = INICIO;
 	flag = false;
 	cont_aux_frases_fichero = INICIO;
 	cont_aux_palabras_fichero = INICIO;
+	red_local=false;
 
 
-	contenido_del_fichero = lee_fichero(fichero);
-	fclose(fichero);
-	
 	//Mediante los siguientes bucles recorremos la tabla que nos ha devuelto la estructura
 	for (cont_aux_frases_fichero=0; 
 		cont_aux_frases_fichero< contenido_del_fichero.num_frases_fichero; 
@@ -74,7 +97,7 @@ int main (){
 			cont_aux_palabras_fichero++){
 
 			//Comprobamos si hemos leido alguna regla de DoS
-			if (strcmp(contenido_del_fichero.contenido_leido_del_fichero[cont_aux_frases_fichero][cont_aux_palabras_fichero], "DoS") == IGUAL){
+			if (strcmp(contenido_del_fichero.contenido_leido_del_fichero[cont_aux_frases_fichero][cont_aux_palabras_fichero], nombre_Coincidencia) == IGUAL){
 				/*-----------------------------------------------------------------------------------------
 					En ese caso, por la estructura del fichero "alert" que genera automáticamente SNORT, 
 					la primera línea será el mensaje que queremos que aparezca y las siguientes 4 líneas 
@@ -102,60 +125,11 @@ int main (){
 			//Comprobamos si hemos detectado una coincidencia y si la línea en la que estamos es la siguiente al mensaje de info
 			if (cont_aux_frases_fichero == linea_coincidencia + SIGUIENTE_LINEA && flag==true) {
 				if (cont_aux_palabras_fichero == POSICION_IP_ORIGEN || cont_aux_palabras_fichero == POSICION_IP_DESTINO){
-					//fprintf(stdout, "%s\n", contenido_del_fichero.contenido_leido_del_fichero[cont_aux_frases_fichero][cont_aux_palabras_fichero]);
-					char **dir_dividida = divide_IP(contenido_del_fichero.contenido_leido_del_fichero[cont_aux_frases_fichero][cont_aux_palabras_fichero]);
-					for (int i = 0; i<TAM_IP; i++)
-						printf("%s\n", dir_dividida[i]);
+					red_local=comprueba_IP(contenido_del_fichero.contenido_leido_del_fichero[cont_aux_frases_fichero][cont_aux_palabras_fichero]);
+					if (red_local == true)
+						fprintf(stdout, "%s\n", contenido_del_fichero.contenido_leido_del_fichero[cont_aux_frases_fichero][cont_aux_palabras_fichero]);
 				}
 			}
 		}
-	}
-	return 0;
-} 	
-
-char ** divide_IP(char *direccion_IP){
-	
-	printf("%s\n", direccion_IP);
-
-	//Varible auxiliar para recorrer caracter a caracter la direccion IP
-	int caracter_dir_IP;
-	//Variable auxiliar para almacenar los dígitos en la posición correcta de la tabla
-	int pos_digito;
-	//Variable auxiliar para almacenar la posicion donde se debe guardar el digito correspondiente
-	//a un fragmento de una direccion IP
-	int pos_aux;
-	//Array para almacenar la dirección IP dividida
-		//4 que son los octetos que contiene una dirección IP
-	char *dir_IP_div[TAM_IP];
-	//Array auxiliar para almacenar el fragmento de la direccion IP
-	char *fragmento_dir_IP = (char *)malloc(sizeof (char)* MAX_DIG_FRAGMENTO_IP);
-
-	caracter_dir_IP = INICIO;
-	pos_digito = INICIO;
-	pos_aux = INICIO;
-
-	for(caracter_dir_IP = 0; caracter_dir_IP <= (int) strlen(direccion_IP); caracter_dir_IP++){
-		
-		
-		
-		if (direccion_IP[caracter_dir_IP] == PUNTO || direccion_IP[caracter_dir_IP] == TERMINADOR){
-			
-			fragmento_dir_IP[pos_aux++] = TERMINADOR;
-			dir_IP_div[pos_digito] = strdup(fragmento_dir_IP);
-			
-			pos_digito++;
-			pos_aux=INICIO;
-		}
-
-		else {
-			fragmento_dir_IP[pos_aux] = direccion_IP[caracter_dir_IP];
-			pos_aux++;
-		}
-		
-		
-	} 
-	for (int i = 0; i<TAM_IP; i++)
-		printf("%s\n", dir_IP_div[i]);
-			
-	return dir_IP_div;
+	}	
 }
