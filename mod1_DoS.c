aa629ed1e09cd6e29be3f3027f8b6e3e3dedd489
fransantosrod <franciscojavier_95@hotmail.com>
2017-02-14 12:43:02 +0100

    Add files via upload

--- mod1_DoS.c
+++ mod1_DoS.c
@@ -80,8 +80,6 @@ void crea_y_escribe_regla(char *nombre_fichero_escritura, struct CONTENIDO_ALERT
 	FILE *fichero_escritura;	
 	//Estructura que almacenará los datos relativos al fichero de las reglas
 	struct CONTENIDO_FICHERO contenido_del_fichero_reglas;
-	//Variable auxiliar para recorrer la tabla devuelta al leer el fichero
-	int cont_aux_linea;
 	
 	//Array auxiliar para ir formando la regla
 		//CAMPOS_REGLAS número de palabras que contendrá como máximo una regla
@@ -93,8 +91,7 @@ void crea_y_escribe_regla(char *nombre_fichero_escritura, struct CONTENIDO_ALERT
 
 	//Inicializamos
 	cont_aux_regla = INICIO;
-	cont_aux_linea = INICIO;
-	coincide = false;
+	coincide = true;
 
 	//Recorremos la estructura de coincidencias que le pasasmos como parámetro
 	for (cont_aux_regla=0; cont_aux_regla<contenido_fichero_alerta.numero_lineas; cont_aux_regla++) {
@@ -108,54 +105,8 @@ void crea_y_escribe_regla(char *nombre_fichero_escritura, struct CONTENIDO_ALERT
 			fclose(fichero_escritura);
 		}
 		
-	
-		//Recorremos la estructura leida del fichero de reglas para buscar coincidencias con la que vamos a introducir
-		//Estaremos aquí mientras que no se haya encontrado ninguna coincidencia
-		for (cont_aux_linea=0; 
-				cont_aux_linea<contenido_del_fichero_reglas.num_frases_fichero && coincide == false; 
-				cont_aux_linea++){
-
-			
-			//Comprobamos si los valores a introducir coinciden con los que ya están
-			/*-----------------------------------------------------------------
-				Aprovechando la estructura de las reglas que es la siguiente:
-					accion protocolo dir_IP_orig puerto_orig ->
-						dir_IP_dest puerto_dest
-				Estos valores serán los que comprobaremos
-			-----------------------------------------------------------------*/
-			if ((strcmp(accion, contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_ACCION]) == IGUAL) &&
-
-			 	(strcmp(contenido_fichero_alerta.protocolo[cont_aux_regla], 
-					contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_PROTOCOLO]) == IGUAL )&&
-
-				(strcmp (contenido_fichero_alerta.dir_IP[cont_aux_regla],
-					contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_DIR_IP_ORIG]) == IGUAL || 
-					strcmp("any", contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_DIR_IP_ORIG]) == IGUAL) &&
-
-				
-				(strcmp (contenido_fichero_alerta.puerto[cont_aux_regla],
-					contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_PUERTO_ORIG]) == IGUAL || 
-					strcmp("any", contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_PUERTO_ORIG]) == IGUAL) &&
-
-				(strcmp (contenido_fichero_alerta.dir_IP[cont_aux_regla],
-					contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_DIR_IP_DEST]) == IGUAL || 
-					strcmp("any", contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_DIR_IP_DEST] )== IGUAL) &&
-
-				
-				(strcmp (contenido_fichero_alerta.puerto[cont_aux_regla],
-					contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_PUERTO_DEST]) == IGUAL ||
-					strcmp("any",  contenido_del_fichero_reglas.contenido_leido_del_fichero[cont_aux_linea][POS_PUERTO_DEST]) == IGUAL)) {
-
-					//Si esto ocurre, estamos ante una coincidencia en la regla
-					coincide = true;
-			}
-
-			else {
-					//En caso contrario estamos ante una nueva regla
-					coincide = false;
-			}
-		}
-
+		coincide = comprueba_Regla(contenido_del_fichero_reglas, contenido_fichero_alerta, accion, cont_aux_regla);
+		
 		//En el caso en el que no encontramos ninguna coincidencia en el fichero
 		if (coincide == false){
 				
@@ -175,11 +126,14 @@ void crea_y_escribe_regla(char *nombre_fichero_escritura, struct CONTENIDO_ALERT
 				if (contenido_fichero_alerta.dir_Con_Puerto[cont_aux_regla] == true){
 					strcat(reglas_aux, contenido_fichero_alerta.puerto[cont_aux_regla]);	
 				}
+				else {
+					strcat(reglas_aux, "any");
+				}
 			}
 			else{
 
 				strcat(reglas_aux, "any");
-				strcat(reglas_aux, " ");
+				strcat(reglas_aux, " ");	
 				strcat(reglas_aux, "any");
 				
 			}
@@ -197,6 +151,10 @@ void crea_y_escribe_regla(char *nombre_fichero_escritura, struct CONTENIDO_ALERT
 				if (contenido_fichero_alerta.dir_Con_Puerto[cont_aux_regla] == true){
 
 					strcat(reglas_aux, contenido_fichero_alerta.puerto[cont_aux_regla]);	
+				}
+				else {
+
+					strcat(reglas_aux, "any");
 				}
 			}
 
@@ -270,8 +228,8 @@ void crea_y_escribe_regla(char *nombre_fichero_escritura, struct CONTENIDO_ALERT
 			//Añadimos el número de revisión, en nuestro caso será siempre 1
 			strcat(reglas_aux, "; rev:1;)");
 			strcat(reglas_aux, "\n");
-			
-	/*		//Abrimos el fichero de las reglas y nos situamos al final de este	
+	/*		
+			//Abrimos el fichero de las reglas y nos situamos al final de este	
 			fichero_escritura = fopen(nombre_fichero_escritura, "a+");
 			
 			//Escribimos la regla que acabamos de crear
@@ -286,5 +244,8 @@ void crea_y_escribe_regla(char *nombre_fichero_escritura, struct CONTENIDO_ALERT
 
 		
 	}
-	
+
+	//Liberamos memoria
+	free(reglas_aux);
 }	
+
