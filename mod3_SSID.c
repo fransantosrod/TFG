b12535b781b3d202c6e7e2f34c17612c750f49d3
fransantosrod <franciscojavier_95@hotmail.com>
2017-02-25 12:20:17 +0100

    Add files via upload

---
+++ mod3_SSID.c
@@ -0,0 +1,27 @@
+/*-------------------------------------------
+Autor: Francisco Javier Santos
+Fecha: 25-02-2017
+Descripción: Fichero que contiene las 
+implementaciones del tercer módulo encargado
+de detectar la suplantación de SSID
+-------------------------------------------*/
+#include <stdio.h>
+
+int main () {
+
+	/*-------------------------------------------
+		Tareas:
+			0. Comprobar si el fichero existe
+			1. Iniciar airodump para capturar y alamcenarlo
+				en un fichero
+			2. Esperar un tiempo para analizar
+			bien el canal
+			3. Para la captura 
+			4. Leer el fichero
+			5. Analizar la lectura
+			6. Tomar acciones si hay alguna coincidencia
+	-------------------------------------------*/
+	printf("Hola mundo\n");
+
+	return 0;
+}
